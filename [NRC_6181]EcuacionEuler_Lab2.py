80d19bc8be9fcead9869882d6e68eb4989033253
Nelson Barahona <nelsonbarahona2011@gmail.com>
2022-05-17 13:38:25 -0500

    Actualizacion 1

---
+++ [NRC_6181]EcuacionEuler_Lab2.py
@@ -0,0 +1,9 @@
+
+h=float(0.1) #valor de h
+
+y0=float(input("ingresa y : ")) #valor inicial a tomar y calcular de Y
+x0=float(input("ingresa x : ")) #valor inicial a tomar y calcular de X
+
+y1=y0+(h)*(x0+2*(x0)*(y0))  #Formula de Eular a calcular
+
+print(y1)    #mostrar proceso
